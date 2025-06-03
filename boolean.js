96ac78627c4f10849ee10232c83543ba6ddc22d7
Arseniy <shagako2007@gmail.com>
2025-06-03 13:47:32 +0300

    lint

--- boolean.js
+++ boolean.js
@@ -1,23 +1,23 @@
-export const isPositive = (num) => num > 0
+export const isPositive = (num) => num > 0;
 
-export const isOdd = (num) => num % 2 !== 0
+export const isOdd = (num) => num % 2 !== 0;
 
-export const checkInequality = (a, b) => a > 2 && b <= 3
+export const checkInequality = (a, b) => a > 2 && b <= 3;
 
-export const checkInequality2 = (a, b) => a >= 0 || b < -2
+export const checkInequality2 = (a, b) => a >= 0 || b < -2;
 
-export const checkBetweenNumbers = (a, b, c) => a < b && b < c
+export const checkBetweenNumbers = (a, b, c) => a < b && b < c;
 
-export const checkOddThreeDigitNumber = (num) => num % 2 !== 0 && num >= 100
+export const checkOddThreeDigitNumber = (num) => num % 2 !== 0 && num >= 100;
 
 export const checkUniqueDigits = (num) => {
 
-}
+};
 
-export const isSecondQuadrant = (x, y) => x < 0 && y > 0
+export const isSecondQuadrant = (x, y) => x < 0 && y > 0;
 
-export const isIsoscelesTriangle = (a, b, c) => (a === b || a === c || b === c) && (a !== 0 || b !== 0 || c !== 0)
+export const isIsoscelesTriangle = (a, b, c) => (a === b || a === c || b === c) && (a !== 0 || b !== 0 || c !== 0);
 
 export const areSameColorFields = (x1, y1, x2, y2) => {
-    
-}
+
+};
--- strings.js
+++ strings.js
@@ -1,92 +1,79 @@
 export const countUppercaseLetters = (text) => {
-    let count = 0
-    for (const char of text) {
-        if (char.toUpperCase() === char.toLowerCase()) {
-            continue
-        }
-        else if (char === char.toUpperCase()) {
-            count += 1
-        }
+  let count = 0;
+  for (const char of text) {
+    if (char.toUpperCase() === char.toLowerCase()) {
+      count += 0;
+    } else if (char === char.toUpperCase()) {
+      count += 1;
     }
-    return count
-}
+  }
+  return count;
+};
 
 export const combineStrings = (n1, n2, str1, str2) => {
-    if (n2 === 0) {
-        return str1.slice(0, n1) + ''
-    }
-    return str1.slice(0, n1) + str2.slice(-n2)
-}
+  if (n2 === 0) {
+    return `${str1.slice(0, n1)}`;
+  }
+  return str1.slice(0, n1) + str2.slice(-n2);
+};
 
-export const containsSubstring = (str, substr) => str.includes(substr)
+export const containsSubstring = (str, substr) => str.includes(substr);
 
-export const replaceSubstring = (str, substr, replacement) => str.replace(substr, replacement)
+export const replaceSubstring = (str, substr, replacement) => str.replace(substr, replacement);
 
-export const countWordsWithSameLetters = (text) => text.length > 1 ? text
-    .toLowerCase()
-    .split(' ')
-    .filter((word) => word[0] === word.at(-1)).length : 0
+export const countWordsWithSameLetters = (text) => (text.length > 1 ? text
+  .toLowerCase()
+  .split(' ')
+  .filter((word) => word[0] === word.at(-1)).length : 0);
 
-export const countWordsWithA = (text) => {
-    let count = 0
-    const words = text.toLowerCase().split(' ')
-    for (const word of words) {
-        if (word.includes('a')) {
-            count += 1
-        }
-    }
-    return count
-}
+export const countWordsWithA = (text) => text.toLowerCase().split(' ').filter((word) => word.includes('a')).length;
 
 export const normalizeSpaces = (text) => {
-    const norm = text.trim()
-    let result = ''
-    for (let i = 0; i < norm.length; i += 1) {
-        if (norm[i] === ' ' && norm[i + 1] === ' ') {
-            continue
-        }
-        result += norm[i]
+  const norm = text.trim();
+  let result = '';
+  for (let i = 0; i < norm.length; i += 1) {
+    if (!(norm[i] === ' ' && norm[i + 1] === ' ')) {
+      result += norm[i];
     }
-    return result
-}
+  }
+  return result;
+};
 
-export const extractFileName = (path) => path.split('/').at(-1).split('.')[0]
+export const extractFileName = (path) => path.split('/').at(-1).split('.')[0];
 
 export const encryptSentence = (text) => {
-    let result = ''
-    for (let i = 1; i < text.length; i += 2) {
-        result += text[i]
+  let result = '';
+  for (let i = 1; i < text.length; i += 2) {
+    result += text[i];
+  }
+  if (text.length % 2 === 0) {
+    for (let i = 2; i <= text.length; i += 2) {
+      result += text.at(-i);
     }
-    if (text.length % 2 === 0) {
-        for (let i = 2; i <= text.length; i += 2) {
-            result += text.at(-i)
-        }
+  } else {
+    for (let i = 1; i <= text.length; i += 2) {
+      result += text.at(-i);
     }
-    else {
-        for (let i = 1; i <= text.length; i += 2) {
-            result += text.at(-i)
-        }
-    }
-    return result
-}
+  }
+  return result;
+};
 
 export const checkBrackets = (str) => {
-    const openingSymbols = ['(', '[', '{', '<']
-    const closingSymbols = [')', ']', '}', '>']
-    const stack = []
-    for (const symbol of str) {
-        if (openingSymbols.includes(symbol)) {
-          stack.push(symbol)
-        }
-        else if (closingSymbols.includes(symbol)) {
-          const pop = stack.pop()
-          if (!pop) {
-            return str.indexOf(symbol)
-          }
-          else if (openingSymbols.indexOf(pop) !== closingSymbols.indexOf(symbol)) {
-            return str.indexOf(symbol)
-          }
-        }
+  const openingSymbols = ['(', '[', '{', '<'];
+  const closingSymbols = [')', ']', '}', '>'];
+  const stack = [];
+  for (const symbol of str) {
+    if (openingSymbols.includes(symbol)) {
+      stack.push(symbol);
+    } else if (closingSymbols.includes(symbol)) {
+      const pop = stack.pop();
+      if (!pop) {
+        return str.indexOf(symbol);
+      }
+      if (openingSymbols.indexOf(pop) !== closingSymbols.indexOf(symbol)) {
+        return str.indexOf(symbol);
+      }
     }
-    return stack.length === 0 ? 0 : -1
-}
+  }
+  return stack.length === 0 ? 0 : -1;
+};
